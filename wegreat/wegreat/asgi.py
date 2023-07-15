1386a6294ccd849e5552ada51f5ad1f694d54271
WilliamAdvance <william@advanceguidance.com>
2023-07-15 13:19:56 +0200

    project init

---
+++ wegreat/wegreat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wegreat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wegreat.settings')
+
+application = get_asgi_application()
